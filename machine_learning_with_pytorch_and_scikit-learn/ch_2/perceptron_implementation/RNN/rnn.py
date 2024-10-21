fa581998d7f358bb39ba7250d1271563404686fa
Biren Mer <birenmmer@gmail.com>
2024-10-21 15:19:19 +0530

    correcting the forward pass and adding proper comments

--- machine_learning_with_pytorch_and_scikit-learn/ch_2/perceptron_implementation/RNN/rnn.py
+++ machine_learning_with_pytorch_and_scikit-learn/ch_2/perceptron_implementation/RNN/rnn.py
@@ -48,76 +48,98 @@ class RNN:
     # Defnining the forward pass
     def forward(self, inputs, hprev):
         """
+        Forward pass through the RNN.
+
         t :timestamp
         hs[t] : hidden state for timestamp t
         os[t] : output for timestamp t
+
+
+        inputs : list of integers (indices) representing the input sequence
+        hprev  : previous hidden state
+        returns:
+            xs   : input vectors (one-hot encoded)
+            hs    : hidden states
+
         """
         xs, hs, os, ycap = {}, {}, {}, {}
         hs[-1] = np.copy(hprev)
         for t in range(len(inputs)):
-            xs[t] = np.zeros_init(self.vocab_size, 1)
+            xs[t] = np.zeros(self.vocab_size, 1)
             xs[t][inputs[t]] = 1  # one hot encoding,1-of-k
+            
+            # Compute the hidden state
             hs[t] = np.tanh(
                 np.dot(self.U, xs[t]) + np.dot(self.W, hs[t - 1]) + self.b
-            )  # Hidden state
+            )  
+            
+            # Compute the output logits
             os[t] = (
                 np.dot(self.V, hs[t]) + self.c
-            )  # unnormalised log probs for next char
-            ycap[t] = self.softmax(os[t])  # applying softmax to get the probability for the next character.
+            )  
+            
+            # Apply softmax to get the probabilities for the next character
+            ycap[t] = self.softmax(
+                os[t]
+            )  
         return xs, hs, ycap
 
     # implementing the softmax function
     def softmax(self, x):
         p = np.exp(x - np.max(x))
         return p / np.sum(p)
-    
-    # defining the loss function 
-    def loss(self,ps,targets):
+
+    # defining the loss function
+    def loss(self, ps, targets):
         """loss of sequence"""
-        #Calculating cross-entropy loss
-        return sum(-np.log(ps[t][targets[t],0] for t in range(self.seq_length)))
+        # Calculating cross-entropy loss
+        return sum(-np.log(ps[t][targets[t], 0] for t in range(self.seq_length)))
+
     # Defining the backward pass / back propogation BPTT
-    def backward(self,xs,hs,ycap,targets):
-        #backward pass computer gradients
-        dU,dW,dV=np.zeros_like(self.U),np.zeros_like(self.W),np.zeros_like(self.V)
-        db,dc=np.zeros_like(self.b),np.zeros_like(self.c)
-        dh_next=np.zeros_like(hs[0])
+    def backward(self, xs, hs, ycap, targets):
+        # backward pass computer gradients
+        dU, dW, dV = np.zeros_like(self.U), np.zeros_like(self.W), np.zeros_like(self.V)
+        db, dc = np.zeros_like(self.b), np.zeros_like(self.c)
+        dh_next = np.zeros_like(hs[0])
 
         for t in reversed(range(self.seq_length)):
-            #Start with output
-            dy=np.copy(ycap[t])
-            
-            #gradient through softmax
-            dy[targets[t]]-=1
+            # Start with output
+            dy = np.copy(ycap[t])
 
-            #Updating dV and dc
-            dV+=np.dot(dy,hs[t].T)
-            dc+=dc
+            # gradient through softmax
+            dy[targets[t]] -= 1
 
-            #dh has two components gradient flowing from output and from next cell
-            dh=np.dot(self.V.T,dy)+dh_next #backprop into h
+            # Updating dV and dc
+            dV += np.dot(dy, hs[t].T)
+            dc += dc
 
-            #dh_rec is the recurring component seen in most of the calculations
-            dh_rec=(1-hs[t]*hs[t])*dh #backprop through tanh non-linearity
-            dh+=dh_rec
+            # dh has two components gradient flowing from output and from next cell
+            dh = np.dot(self.V.T, dy) + dh_next  # backprop into h
 
-            #Updating dU and dW
-            dU+=np.dot(dh_rec,xs[t].T)
-            dW+=np.dot(dh_rec,hs[t-1].T)
+            # dh_rec is the recurring component seen in most of the calculations
+            dh_rec = (1 - hs[t] * hs[t]) * dh  # backprop through tanh non-linearity
+            dh += dh_rec
+
+            # Updating dU and dW
+            dU += np.dot(dh_rec, xs[t].T)
+            dW += np.dot(dh_rec, hs[t - 1].T)
 
             # pass the gradient fromt the next cell for next iteration
-            dh_next=np.dot(self.W.T,dh_rec)
+            dh_next = np.dot(self.W.T, dh_rec)
 
         # Clipping weights to avoid gradient explosion.
-        for d_param in [dU,dW,dV,db,dc]:
-            np.clip(d_param,-5,5,out=d_param)
-        return dU,dW,dV,db,dc
+        for d_param in [dU, dW, dV, db, dc]:
+            np.clip(d_param, -5, 5, out=d_param)
+        return dU, dW, dV, db, dc
+
     """
     Using BPTT we calculated the gradient for each parameter of the model. it is now time to update the weights.
     """
+
     # Defining the weight update function
-    def update_model(self,dU,dV,dW,db,dc):
-        for param,d_param in zip([self.U,self.W,self.V,self.b,self.c],[dU,dV,dW,db,dc]):
-            #Changing paramerters according to gradients and learning rate
-            param+=-self.learning_rate*d_param
-            
+    def update_model(self, dU, dV, dW, db, dc):
+        for param, d_param in zip(
+            [self.U, self.W, self.V, self.b, self.c], [dU, dV, dW, db, dc]
+        ):
+            # Changing paramerters according to gradients and learning rate
+            param += -self.learning_rate * d_param
