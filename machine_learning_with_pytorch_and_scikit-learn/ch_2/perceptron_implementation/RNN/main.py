c2a8332ff86ad81acedec94fc7a1f4fd704ab908
Biren Mer <birenmmer@gmail.com>
2024-10-23 00:49:32 +0530

    changes in main.py

---
+++ machine_learning_with_pytorch_and_scikit-learn/ch_2/perceptron_implementation/RNN/main.py
@@ -0,0 +1,14 @@
+from train import train_rnn
+from load import load_rnn
+
+training_file_path="/home/wifee/workspace/learning/ML_book/Mobi_dick_book.txt"
+
+if __name__ == "__main__":
+    model,data_reader = train_rnn(training_file_path)
+    # Save model is done inside train_rnn, but you can load later for prediction
+    # rnn = load_rnn("rnn_model.pkl")
+    
+    # Example of making a prediction
+    start_text = "Hello"
+    predicted_text = model.predict(data_reader, start_text, n=200)
+    print(predicted_text)
