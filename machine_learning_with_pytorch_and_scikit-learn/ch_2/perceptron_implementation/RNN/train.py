80c96bd3a03e4791beec8dffba245029ac30f336
Biren Mer <birenmmer@gmail.com>
2024-10-23 00:32:21 +0530

    adding datareader to the pickle file

--- machine_learning_with_pytorch_and_scikit-learn/ch_2/perceptron_implementation/RNN/train.py
+++ machine_learning_with_pytorch_and_scikit-learn/ch_2/perceptron_implementation/RNN/train.py
@@ -1,6 +1,6 @@
 import pickle
-from data_reader import DataReader
 from rnn import RNN
+from data_reader import DataReader
 
 def train_rnn(training_file_path, seq_length=25, hidden_size=100, learning_rate=1e-1, model_save_path="rnn_model.pkl"):
     # read text from the training file
@@ -17,8 +17,8 @@ def train_rnn(training_file_path, seq_length=25, hidden_size=100, learning_rate=
     # train the model
     rnn.train(data_reader)
     
-    # save the model using pickle
+    # save the model and data_reader using pickle
     with open(model_save_path, 'wb') as f:
-        pickle.dump(rnn, f)
+        pickle.dump((rnn, data_reader), f)
     
     return rnn
