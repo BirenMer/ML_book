4b673d9f7b08471f684c346f0547e666409699be
Biren Mer <birenmmer@gmail.com>
2024-10-23 10:55:01 +0530

    also calling data_reader.close to make sure file is closed when we save the model so we don't face any errors

--- machine_learning_with_pytorch_and_scikit-learn/ch_2/perceptron_implementation/RNN/train.py
+++ machine_learning_with_pytorch_and_scikit-learn/ch_2/perceptron_implementation/RNN/train.py
@@ -2,23 +2,20 @@ import pickle
 from rnn import RNN
 from data_reader import DataReader
 
-def train_rnn(training_file_path, seq_length=25, hidden_size=100, learning_rate=1e-1, model_save_path="rnn_model.pkl"):
-    # read text from the training file
-    data_reader = DataReader(training_file_path, seq_length)
+def train_rnn(training_file_path, seq_length=25, hidden_size=100, learning_rate=1e-1):
+    # Read text from the "input.txt" file
+    data_reader = DataReader(f"{training_file_path}", seq_length)
     
-    # create the RNN model
-    rnn = RNN(
-        hidden_size=hidden_size,
-        vocab_size=data_reader.vocab_size,
-        seq_length=seq_length,
-        learning_rate=learning_rate,
-    )
+    rnn = RNN(hidden_size=hidden_size, vocab_size=data_reader.vocab_size, seq_length=seq_length, learning_rate=learning_rate)
     
-    # train the model
+    # Train the RNN
     rnn.train(data_reader)
+
+    # Close the data reader file before saving
+    data_reader.close()
     
-    # save the model and data_reader using pickle
-    with open(model_save_path, 'wb') as f:
+    # Save the trained model and data reader
+    with open("trained_model.pkl", "wb") as f:
         pickle.dump((rnn, data_reader), f)
-    
+
     return rnn
