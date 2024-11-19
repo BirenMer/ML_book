3281b83cfdeea2da7e22610ef025dbbd87cc0046
Biren Mer <birenmmer@gmail.com>
2024-11-19 17:26:25 +0530

    creating dense layers in the main file

--- LSTM/main.py
+++ LSTM/main.py
@@ -1,6 +1,7 @@
 import numpy as np
 import matplotlib.pyplot as plt
 from LSTM import LSTM
+from dense_layer import DenseLayer
 
 #Creating data 
 
@@ -24,11 +25,25 @@ lstm=LSTM(n_neurons)
 lstm.forward(X_t)
 
 #Creating a plot to see the code working CKP-1
-for h in lstm.H:
-    plt.plot(np.arange(20),h[0:20],'k-',linewidth=1,alpha=0.05)
+# for h in lstm.H:
+#     plt.plot(np.arange(20),h[0:20],'k-',linewidth=1,alpha=0.05)
 
 
-for c in lstm.C:
-    plt.plot(np.arange(20),h[0:20],'k-',linewidth=1,alpha=0.05)
+# for c in lstm.C:
+#     plt.plot(np.arange(20),h[0:20],'k-',linewidth=1,alpha=0.05)
 # plt.show()
+
+#Adding full back prop CKP-2
+T=max(X_t.shape)
+# no of features in our case is the number of neurons in the first layer
+dense1=DenseLayer(n_neurons,T)
+
+#Note the number of neurons in the first layer has to be same as the number of inputs in the second layer 
+
+# Setting up second dense layer which can convert the inpput into prediction Y hat
+dense2=DenseLayer(T,1)
+
+
+
+
 
