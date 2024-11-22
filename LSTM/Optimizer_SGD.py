0c0ce7edafa923df3d940965ddb8b8e8a5debdda
Biren Mer <birenmmer@gmail.com>
2024-11-22 15:04:58 +0530

    remaning the LSTM optimizer and adding the SGD optimizer

--- LSTM/Optimizer_SGD.py
+++ LSTM/Optimizer_SGD.py
@@ -1,6 +1,6 @@
 import numpy as np
 
-class OptimizerSGD:
+class OptimizerSGDLSTM:
     def __init__(self,learning_rate=1,decay=0,momentum=0) -> None:
         self.learning_rate=learning_rate
         self.current_learning_rate=learning_rate
@@ -99,3 +99,37 @@ class OptimizerSGD:
 
     def post_update_params(self):
         self.iterations+=1
+
+class OptimizerSGD:
+
+        def __init__(self,learning_rate=1e-5,decay=0,momentum=0) -> None:
+            self.learning_rate=learning_rate
+            self.current_learning_rate=learning_rate
+            self.decay=decay
+            self.iterations=0
+            self.momentum=momentum
+
+        def pre_update_params(self):
+            if self.decay:
+              self.current_learning_rate=self.learning_rate*(1/(1+self.decay*self.iterations))
+
+        def update_params(self,layer):
+
+            if self.momentum:
+                if not hasattr(layer,'weight_momentums'):
+                  layer.weight_momentums=np.zeros_like(layer.weights)
+                  layer.bais_momentums=np.zeros_like(layer.baises)
+                weight_updates=self.momentum*layer.weight_momentums-self.current_learning_rate*layer.dweights
+                layer.weight_momentums=weight_updates
+
+                bias_updates=self.momentum*layer.bias_momentums-self.current_learning_rate*layer.dbaises
+                layer.bias_momentums=bias_updates
+
+            else:
+                weight_updates=-self.current_learning_rate*layer.dweights
+                bias_updates=-self.current_learning_rate*layer.dbaises
+            layer.weights+=weight_updates
+            layer.biases+=bias_updates
+        
+        def post_update_params(self):
+            self.iterations+=1
