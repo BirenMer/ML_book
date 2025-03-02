4215b3226c2a436d85de6580c8a79c055d4c0e70
Biren Mer <birenmmer@gmail.com>
2025-03-02 14:22:37 +0530

    add return statments

--- LSTM_text_prediction/activation_function/Tanh.py
+++ LSTM_text_prediction/activation_function/Tanh.py
@@ -4,7 +4,9 @@ class Tanh:
     def forward(self,inputs):
         self.output=np.tanh(inputs)
         self.inputs=inputs
+        return self.output
+        
     def backward(self,dvalues):
         deriv=1-self.output**2
         self.dinputs=np.multiply(deriv,dvalues)
-        
+        return self.dinputs 
