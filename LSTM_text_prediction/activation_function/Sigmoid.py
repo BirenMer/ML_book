9c785d4b61a5ff57363087df4b73f3672dfa95f4
Biren Mer <birenmmer@gmail.com>
2025-03-02 14:23:06 +0530

    add return statement

--- LSTM_text_prediction/activation_function/Sigmoid.py
+++ LSTM_text_prediction/activation_function/Sigmoid.py
@@ -4,7 +4,7 @@ class Sigmoid:
         sigm=np.clip(1/(1+np.exp(-M)),1e-7,1-1e-7)
         self.output=sigm
         self.inputs=sigm #needed for back prop
-
+        return self.output
     def backward(self,dvalues):
         sigm=self.inputs
         deriv=np.multiply(sigm,(1-sigm))
