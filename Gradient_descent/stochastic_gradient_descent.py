840c9488fd8430c25ce80d7c35bda87eccdf9f60
Biren Mer <birenmmer@gmail.com>
2025-03-09 17:30:42 +0530

    add stochstic gradient descent

---
+++ Gradient_descent/stochastic_gradient_descent.py
@@ -0,0 +1,41 @@
+#WIP code
+import numpy as np
+#Sigmoid function f(x)
+
+#datapoints
+X=[0.5,2.5]
+Y=[0.2,0.0]
+
+
+def f(w,b,x):
+    return 1.0/(1.0 * np.exp(-(w*x+b)))
+
+
+#Error function
+def error(w,b):
+    err=0.0
+    for x,y in zip(X,Y):
+        fx=f(w,b,x)
+        err+=0.5*(fx-y)**2
+    return err
+
+
+def grad_b(w,b,x,y):
+    fx=f(w,b,x)
+    return (fx-y) * (fx) * (1-fx)
+
+def grad_w(w,b,x,y):
+    fx=f(w,b,x)
+    return (fx-y) * (fx) * (1-fx) * x
+#this code updates the parameters at evey point,
+def do_stochastic_gragient_descent():
+    w,b,eta,max_epochs=-2,-2,1.0,1000
+    for i in range(max_epochs):
+        dw,db=0,0
+        for x,y in zip(X,Y):
+            dw+=grad_w(w,b,x,y)
+            db+=grad_b(w,b,x,y)
+            #Updating parameters at each and evey data point unlike vanilla GD
+            w=w-eta*dw
+            b=b-eta*db
+        # print(w,b)
