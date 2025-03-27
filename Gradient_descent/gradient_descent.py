3c7ffa484365fb614f7374d9aac1acb4c542fc4e
Biren Mer <birenmmer@gmail.com>
2025-03-28 02:21:34 +0530

    update gradient descent

--- Gradient_descent/gradient_descent.py
+++ Gradient_descent/gradient_descent.py
@@ -1,46 +1,32 @@
 #vannila gradient descent
 import numpy as np
 
+from general_utils import data_points, grad_w,grad_b,error
 from trajectory_plotting_utils import plot_error_contour
 
-# Data points
-X = [0.5, 2.5]
-Y = [0.2, 0.0]
 
-# Sigmoid function
-def f(w, b, x):
-    return 1.0 / (1.0 + np.exp(-(w * x + b)))
-
-# Error function
-def error(w, b):
-    err = 0.0
-    for x, y in zip(X, Y):
-        fx = f(w, b, x)
-        err += 0.5 * (fx - y) ** 2
-    return err
-
-# Gradients
-def grad_b(w, b, x, y):
-    fx = f(w, b, x)
-    return (fx - y) * fx * (1 - fx)
-
-def grad_w(w, b, x, y):
-    fx = f(w, b, x)
-    return (fx - y) * fx * (1 - fx) * x
 
 def do_gragient_descent():
-  
+    #trying to converge gradient descent for the below two point i.e. 
+    # GD will optimized it self to reach convergence while trying to include this points in it trajectory. 
+    # Data points
+    X,Y=data_points()
+
     w,b,eta,max_epochs=-2,-2,1.0,1000
+
     trajectory = [(w, b)]
+
     for i in range(max_epochs):
         dw,db=0,0
         for x,y in zip(X,Y):
             dw+=grad_w(w,b,x,y)
             db+=grad_b(w,b,x,y)
+            
         #updating parameters after going thorugh each and every data point
         w=w-eta*dw
         b=b-eta*db
         trajectory.append((w,b))
+
     return trajectory
 
 gradient_descent_trajectory=do_gragient_descent()
