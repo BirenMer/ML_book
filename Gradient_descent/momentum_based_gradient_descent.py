d0c57aad92f7bb568751af436e4eab4e1f1ce08e
Biren Mer <birenmmer@gmail.com>
2025-05-10 03:41:50 +0530

    update momentum based GD file

--- Gradient_descent/momentum_based_gradient_descent.py
+++ Gradient_descent/momentum_based_gradient_descent.py
@@ -1,34 +1,39 @@
-#WIP code
 import numpy as np
-#Sigmoid function f(x)
-
-from trajectory_plotting_utils import plot_error_contour
+from trajectory_plotting_utils import plot_contour
 from general_utils import data_points, error, grad_w,grad_b
 
-def do_momentum_based_gradient_descent(init_w,init_b,max_epochs):
-    X,Y=data_points()
-    w,b,eta = init_w,init_b,1.0
-    
-    trajectory=[(w,b)]
+def do_momentum_based_gradient_descent(eta=0.1, max_epochs=1000):
+    X, Y = data_points()
+    w, b = -2, 2
+    trajectory = [(w, b)]
 
-    prev_v_w,prev_v_b,gamma=0,0,0.9
+    prev_v_w, prev_v_b = 0, 0
+    gamma = 0.9
+
     for i in range(max_epochs):
-        dw,db=0,0
-        for x,y in zip(X,Y):
-            dw+=grad_w(w,b,x,y)
-            db+=grad_b(w,b,x,y)
-        v_w=gamma*prev_v_w+eta*dw
-        v_b=gamma*prev_v_b+eta*db
-        
-        w=w*v_w
-        b=b*v_b
+        dw, db = 0, 0
+        for x, y in zip(X, Y):
+            dw += grad_w(w, b, x, y)
+            db += grad_b(w, b, x, y)
 
-        trajectory.append((w,b))
-        prev_v_w=v_w
-        prev_v_b=v_b
-        
-        return trajectory
+        # Optionally average gradients
+        dw /= len(X)
+        db /= len(X)
 
-momentum_based_gradient_descent_trajectory=do_momentum_based_gradient_descent(-2,-2,1000)
+        # Momentum update
+        v_w = gamma * prev_v_w + eta * dw
+        v_b = gamma * prev_v_b + eta * db
 
-plot_error_contour(error_function=error,trajectory=momentum_based_gradient_descent_trajectory,algo_name="Momentum Based GD")
+        w -= v_w
+        b -= v_b
+
+        trajectory.append((w, b))
+
+        prev_v_w = v_w
+        prev_v_b = v_b
+
+    return trajectory
+
+trajectory=do_momentum_based_gradient_descent()
+
+plot_contour(trajectory,label="Momentum GD Path")
