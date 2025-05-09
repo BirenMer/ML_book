081d45c54e31c689e82f1432856d8a60e427cb44
Biren Mer <birenmmer@gmail.com>
2025-05-10 04:06:47 +0530

    update Stochastic Momentum Based GD to plot trajectory

--- Gradient_descent/stochastic_momentum_based_gradient_descent.py
+++ Gradient_descent/stochastic_momentum_based_gradient_descent.py
@@ -1,45 +1,32 @@
-#WIP code
 import numpy as np
-#Sigmoid function f(x)
 
-#datapoints
-X=[0.5,2.5]
-Y=[0.2,0.0]
+from trajectory_plotting_utils import plot_contour
+from general_utils import data_points, grad_w,grad_b
 
+def do_stochastic_momentum_based_gradient_descent(init_w=-2, init_b=2, lr=0.1, max_epochs=1000):
+    w, b = init_w, init_b
+    eta = lr
+    gamma = 0.9
+    prev_v_w, prev_v_b = 0, 0
+    X, Y = data_points()
+    trajectory = [(w, b)]
 
-def f(w,b,x):
-    return 1.0/(1.0 * np.exp(-(w*x+b)))
+    for epoch in range(max_epochs):
+        for x, y in zip(X, Y):
+            dw = grad_w(w, b, x, y)
+            db = grad_b(w, b, x, y)
 
+            v_w = gamma * prev_v_w + eta * dw
+            v_b = gamma * prev_v_b + eta * db
 
-#Error function
-def error(w,b):
-    err=0.0
-    for x,y in zip(X,Y):
-        fx=f(w,b,x)
-        err+=0.5*(fx-y)**2
-    return err
+            w = w - v_w
+            b = b - v_b
 
+            prev_v_w = v_w
+            prev_v_b = v_b
 
-def grad_b(w,b,x,y):
-    fx=f(w,b,x)
-    return (fx-y) * (fx) * (1-fx)
+            trajectory.append((w, b))
 
-def grad_w(w,b,x,y):
-    fx=f(w,b,x)
-    return (fx-y) * (fx) * (1-fx) * x
-
-def do_stochastic_momentum_based_gradient_descent(init_w,init_b,max_epochs):
-    w,b,eta = init_w,init_b,1.0
-    prev_v_w,prev_v_b,gamma=0,0,0.9
-    for i in range(max_epochs):
-        dw,db=0,0
-        for x,y in zip(X,Y):
-            dw+=grad_w(w,b,x,y)
-            db+=grad_b(w,b,x,y)
-            v_w=gamma*prev_v_w+eta*dw
-            v_b=gamma*prev_v_b+eta*db
-            w=w*v_w
-            b=b*v_b
-            prev_v_w=v_w
-            prev_v_b=v_b
-        
+    return trajectory
+trajectory=do_stochastic_momentum_based_gradient_descent()
+plot_contour(trajectory=trajectory,label="Stochastic Momentum Based GD")
